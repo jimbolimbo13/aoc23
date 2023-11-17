096f3f553813968a2bc8b2655c490054d4cde5dd
jimbolimbo13 <jim@beatty.family>
2023-11-16 23:29:02 -0500

    Script and template for copying each day

---
+++ new-day.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+if [ "$#" -eq 0 ] ; then
+    echo "What day is it?"
+    read today
+else
+    today=$1
+fi
+
+mkdir -p "day$today"
+cp template/dayXXX.esx "day$today/day$today.esx"
+touch "day$today/test-day"$today".txt"
+touch "day$today/input-day"$today".txt"
+
+sed -i "s/XXX/$today/g" day$today/*
