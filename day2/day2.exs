d58577c0eefb28a381b57bbb0c9b0b0b8bbe1f72
jimbolimbo13 <jim@beatty.family>
2023-12-02 23:20:35 -0500

    Day2 part2

--- day2/day2.exs
+++ day2/day2.exs
@@ -1,22 +1,16 @@
 IO.puts("Advent of Code Day2")
 
-defmodule Day2 do
+defmodule Day2Part1 do
+
   def load_file(f_name) do
     file_lines = String.split(File.read!(f_name), "\n")
 
     part1 = Enum.map(file_lines, &valid_line/1)
     |> Enum.reduce(0, fn x, acc -> x + acc end)
-    #part2 =
 
     IO.puts("Day2 part1: #{part1}")
-    #IO.puts("Day2 part2: #{part2}")
  end
 
-  # def parse_line(line) do
-  #  String.split(line, ":")
-  #  |> List.last()
-  #  |> String.split(";")
-  # end
 
   def valid_line("") do
    0
@@ -60,9 +54,67 @@ defmodule Day2 do
 
      end
   end
+end
+
+defmodule Day2Part2 do
+
+  def load_file(f_name) do
+    file_lines = String.split(File.read!(f_name), "\n")
+
+    part2 = Enum.map(file_lines, &count_line/1)
+    |> Enum.reduce(0, fn x, acc -> x + acc end)
+
+    IO.puts("Day2 part2: #{part2}")
+ end
+
+  def count_line("") do
+   0
+  end
+
+  def count_line(line) do
+   [_, cubes] = String.split(line, ":")
+   rounds = String.split(cubes, [",", ";"])
+   maxes = %{blue: 0, green: 0, red: 0}
+   maxes = round_max(rounds, maxes)
+   round_total(maxes)
+  end
+
+  def round_max([], maxes) do
+   maxes
+  end
+
+  def round_max(rounds, maxes) do
+   [round | tail] = rounds
+   [count_str, color] = String.split(round)
+   {count, _} = Integer.parse(count_str)
+   maxes =  case String.trim(color) do
+     "blue" -> case Map.fetch(maxes, :blue) do
+                 {:ok, val} -> %{maxes | blue: max(val, count)}
+                 :error -> Map.put(maxes, :blue, count)
+               end
+
+     "green" -> case Map.fetch(maxes, :green) do
+                 {:ok, val} -> Map.put(maxes, :green, max(val, count) )
+                 :error -> Map.put(maxes, :green, count)
+                end
+     "red" -> case Map.fetch(maxes, :red) do
+                 {:ok, val} -> Map.put(maxes, :red, max(val, count) )
+                 :error -> Map.put(maxes, :red, count)
+                end
+   end
+   round_max(tail, maxes)
+  end
+
+  def round_total(maxes) do
+   {:ok, red} = Map.fetch(maxes, :red)
+   {:ok, blue} = Map.fetch(maxes, :blue)
+   {:ok, green} = Map.fetch(maxes, :green)
+   red * blue * green
+  end
 
 end
 
 file_name = "input-day2.txt"
 #file_name = "test-day2.txt"
-Day2.load_file(file_name)
+Day2Part1.load_file(file_name)
+Day2Part2.load_file(file_name)
