639098e8b21613b37a29089cfc24165a6c3666d2
jimbolimbo13 <jim@beatty.family>
2023-12-06 23:23:30 -0500

    Day4 part2

--- day4/day4.exs
+++ day4/day4.exs
@@ -7,11 +7,11 @@ defmodule Day4 do
     part1 = Enum.map(file_lines, fn x -> String.split(List.last(String.split(x ,":")), ["|", " "]) end )
     |> Enum.map(&count_pow/1)
     |> Enum.sum()
-
-    #part2 =
-
     IO.puts("Day4 part1: #{part1}")
-    #IO.puts("Day4 part2: #{part2}")
+
+    part2_map = Enum.map(file_lines, &part2_split/1)
+    part2 = duplicate(part2_map)
+    IO.puts("Day4 part2: #{part2}")
  end
 
   def count_pow(list) do
@@ -25,8 +25,49 @@ defmodule Day4 do
 
    end
 
-end
+  def part2_split(line) do
+   case String.split(line, ":") do
+     [_card, draw] -> %{count: 1, draw: draw}
+     [""] -> %{count: 0, draw: ""}
+   end
 
-#file_name = "input-day4.txt"
-file_name = "test-day4.txt"
+  end
+
+  def card_score(card_to_score) do
+   card = Map.fetch!(card_to_score, :draw)
+   int_list = String.split(card, ["|", " "])
+   |> Enum.reject(fn x -> x == "" end)
+
+   length(int_list) - length(Enum.uniq(int_list))
+  end
+
+  def add_count(card_map, count) do
+   Map.update!(card_map, :count, fn x -> x + count end)
+  end
+
+  def duplicate([]) do
+   0
+  end
+
+  def duplicate(all_lines) do
+   [head | tail] = all_lines
+
+   score = card_score(head)
+   count = Map.fetch!(head, :count)
+   tail = if score > 0 do
+    to_add = Enum.take(tail, score)
+    |> Enum.map(fn x -> add_count(x, count) end)
+    remaining = Enum.take(tail, (score - length(tail)))
+
+    List.flatten(to_add, remaining)
+    else
+      tail
+   end
+   count + duplicate(tail)
+  end
+
+ end
+
+file_name = "input-day4.txt"
+#file_name = "test-day4.txt"
 Day4.load_file(file_name)
